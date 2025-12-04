68dce4614c9edc3a43370c71d8248b128014fbaf
Damon <damon.dinh@hotmail.com>
2025-12-04 18:19:12 +0800

    Control Flow in Java - PROJECT: Todo List

--- WEB-DEV-COURSE/06section/lesson/03module/project/script.js
+++ WEB-DEV-COURSE/06section/lesson/03module/project/script.js
@@ -5,5 +5,20 @@ while (true) {
     let task = prompt("Enter a task (or type 'done' to finish:");
 
     // validate user input for done
-    if()
+    if( task.toLowerCase() === "done") {
+        break; // if done, break out of while loop
+    }
+
+    tasks.push(task); // adding user input to tasks
 }
+
+// displaying tasks
+console.log("Your Todo List:");
+
+// for (let i = 0; i < tasks.length; i++) {
+//     console.log(`${i + 1}. ${tasks[i]}`);
+// }
+
+tasks.forEach((task, index) => {
+    console.log(`${index + 1}. ${task}`);
+});
