e89093b59346b690763be8177b22bc319b4e38df
Damon <damon.dinh@hotmail.com>
2025-12-04 19:02:13 +0800

    Arrays and Objects - Declarin and Manipulating Array + Determining Size of an Array

---
+++ WEB-DEV-COURSE/06section/lesson/05module/arrays.js
@@ -0,0 +1,11 @@
+const fruits = ["Apple", "Banana", "Cherry"];
+console.log(fruits);
+
+console.log(fruits[0]);
+console.log(fruits[2]);
+
+fruits[1] = "Blueberry";
+
+console.log(fruits);
+
+console.log(fruits.length);
