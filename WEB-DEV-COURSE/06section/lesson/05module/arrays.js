142f1ed3ea4bcf42308d6c773405cee9de175a28
Damon <damon.dinh@hotmail.com>
2025-12-04 19:05:52 +0800

    Arrays and Objects - Array Methods: P

--- WEB-DEV-COURSE/06section/lesson/05module/arrays.js
+++ WEB-DEV-COURSE/06section/lesson/05module/arrays.js
@@ -1,11 +1,27 @@
 const fruits = ["Apple", "Banana", "Cherry"];
+// console.log(fruits);
+
+// console.log(fruits[0]);
+// console.log(fruits[2]);
+
+// fruits[1] = "Blueberry";
+
+// console.log(fruits);
+
+// console.log(fruits.length);
+
+// Array Methods
+fruits.push("Mango");
+
 console.log(fruits);
 
-console.log(fruits[0]);
-console.log(fruits[2]);
-
-fruits[1] = "Blueberry";
+fruits.pop();
 
 console.log(fruits);
 
-console.log(fruits.length);
+fruits.shift();
+
+console.log(fruits);
+
+fruits.unshift("Grapes");
+console.log(fruits);
