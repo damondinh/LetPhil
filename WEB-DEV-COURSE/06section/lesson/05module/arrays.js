087275a0c1d29897961d8cc59053160c7d796762
Damon <damon.dinh@hotmail.com>
2025-12-04 19:09:16 +0800

    Arrays and Objects - Array Methods: Slice

--- WEB-DEV-COURSE/06section/lesson/05module/arrays.js
+++ WEB-DEV-COURSE/06section/lesson/05module/arrays.js
@@ -11,17 +11,32 @@ const fruits = ["Apple", "Banana", "Cherry"];
 // console.log(fruits.length);
 
 // Array Methods
+// push
 fruits.push("Mango");
 
 console.log(fruits);
 
+// pop
 fruits.pop();
 
 console.log(fruits);
 
+// shift - removes from beginning
 fruits.shift();
 
 console.log(fruits);
 
+// unshift - adds to beginning
 fruits.unshift("Grapes");
+
 console.log(fruits);
+
+// splice - add / remove elements
+fruits.splice(1,1, "Peach");
+
+console.log(fruits);
+
+fruits.splice(1, 2);
+
+console.log(fruits);
+
