a5cfba76c33044041e43cb4c5515f720c312203c
Damon <damon.dinh@hotmail.com>
2025-12-04 18:38:45 +0800

    Functions - Arrow Functions and Function Expressions

--- WEB-DEV-COURSE/06section/lesson/04module/script.js
+++ WEB-DEV-COURSE/06section/lesson/04module/script.js
@@ -1,5 +1,30 @@
-function greetUser(name) {
-    console.log(`Hello, ${name}!`);
-}
+// function greetUser(name) {
+//     console.log(`Hello, ${name}!`);
+// }
 
-greetUser("Alice");
+// greetUser("Alice");
+
+// function addNumbers(num1, num2, num3) {
+//     console.log(`Sum: ${num1 + num2 + num3}`);
+// }
+
+// addNumbers(1,2,3);
+
+const greet = function(name) {
+    return `Hello, ${name}`;
+};
+
+console.log(greet("Alice"));
+
+// Arrow functions ( Short Syntax )
+
+// const square = (num) => num**2;
+
+// const square = (num) => {
+//     return num**2
+// };
+// console.log(square(5));
+
+// () => {}
+const multiply = (a, b) => a * b;
+console.log(multiply(3,4));
