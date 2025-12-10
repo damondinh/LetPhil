0cfc33d0894637a44d363f27d9760fdfb21e3f96
Damon <damon.dinh@hotmail.com>
2025-12-10 19:38:37 +0800

    mini-project-operators complete

--- WEB-DEV-COURSE-LVL-2/practice-homework/mini-project-Operators-ExpressionPlayground/mentee/script.js
+++ WEB-DEV-COURSE-LVL-2/practice-homework/mini-project-Operators-ExpressionPlayground/mentee/script.js
@@ -41,3 +41,81 @@ STEP 4 — Render
 STEP 5 — Handler
   When runBtn is clicked, read a and b, compute, and render.
 */
+
+let aInput = document.getElementById("aInput");
+let bInput = document.getElementById("bInput");
+let runBtn = document.getElementById("runBtn");
+let resultList = document.getElementById("resultList");
+
+// converts a text input’s value to a number and returns the number.
+function readNumber(textInput) {
+  return parseInt(textInput);
+}
+
+// takes numbers a and b and returns an object with keys in this exact list and order:
+function computAll(aInput,bInput) {
+  let a = readNumber(aInput);
+  let b = readNumber(bInput);
+
+  let sum = a + b;
+
+  let diff = a - b;
+
+  let prod = a * b;
+
+  let quot = a / b;
+
+  let mod = a % b;
+
+  let eqLoose = a == b;
+
+  let eqStrict = a === b;
+  
+  let greater = a > b;
+
+  let bothEven = (a % 2 === 0) && (b % 2 === 0);
+
+  let anyOver10 = (a > 10) || (b > 10);
+
+  let notEqual = a !== b;
+  
+  let precOne = a + b * 2;
+
+  let precTwo = (a + b) * 2;
+
+  let results = {
+    sum: sum, 
+    diff: diff,
+    prod: prod, 
+    quot: quot, 
+    mod: mod,
+    eqLoose: eqLoose, 
+    eqStrict: eqStrict, 
+    greater: greater,
+    bothEven: bothEven, 
+    anyOver10: anyOver10, 
+    notEqual: notEqual,
+    precOne: precOne,
+    precTwo: precTwo
+  }
+
+  return results;
+}
+
+function renderResults(obj) {
+  let outputList = [];
+
+  for (const [key, value] of Object.entries(obj)) {
+    outputList.push(`${key}: ${value}`);
+  }
+
+  return outputList;
+}
+
+// Start
+runBtn.addEventListener('click', function() {
+
+  let resultsObj = computAll(aInput.value,bInput.value);
+
+  resultList.textContent = renderResults(resultsObj);
+});
