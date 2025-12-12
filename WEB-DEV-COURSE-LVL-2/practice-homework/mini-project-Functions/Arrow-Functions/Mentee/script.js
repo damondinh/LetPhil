87aa364f0bdc9ccf7c03fb54400d3aba19ad562a
Damon <damon.dinh@hotmail.com>
2025-12-12 21:02:38 +0800

    mini-project-functions arrow-functions complete

--- WEB-DEV-COURSE-LVL-2/practice-homework/mini-project-Functions/Arrow-Functions/Mentee/script.js
+++ WEB-DEV-COURSE-LVL-2/practice-homework/mini-project-Functions/Arrow-Functions/Mentee/script.js
@@ -1,11 +1,15 @@
 // Mentee Version — Steps Only (same steps appear in Mentor)
 
 // STEP 1 — Make a constant addArrow assigned to an arrow function that returns a+b.
-
+const addArrow = (a, b) => a + b
 // STEP 2 — Make a constant squareArrow that returns n*n (use concise one-line form).
-
+const squareArrow = (n) => n*n
 // STEP 3 — Make a constant describeArrow that takes (name, city) and returns a sentence.
-
+const describeArrow = (name, city) => name + " " + city + " sentence";
 // STEP 4 — Make a constant maxArrow that returns the max of two numbers using a conditional (?:).
-
+const maxArrow = (a, b) => (a > b) ? a : b;
 // STEP 5 — TEST: Log calls to each arrow function with sample values.
+console.log(addArrow(5,10));
+console.log(squareArrow(5));
+console.log(describeArrow("john","melbourne"));
+console.log(maxArrow(5,10));
