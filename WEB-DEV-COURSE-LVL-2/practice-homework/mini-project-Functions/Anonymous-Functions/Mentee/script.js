28242718923d07ab90483f4170fcc7ae5cd55848
Damon <damon.dinh@hotmail.com>
2025-12-12 20:11:30 +0800

    mini-project-functions anonymous-functions complete

--- WEB-DEV-COURSE-LVL-2/practice-homework/mini-project-Functions/Anonymous-Functions/Mentee/script.js
+++ WEB-DEV-COURSE-LVL-2/practice-homework/mini-project-Functions/Anonymous-Functions/Mentee/script.js
@@ -9,3 +9,30 @@
 // STEP 4 — Use filter with an anonymous function to keep only strings longer than 3 letters from ['pen','book','AI','tool'].
 
 // STEP 5 — Use reduce with an anonymous function to sum an array; log the result.
+
+setTimeout (function() {
+    console.log("Done!");
+}, 500);
+
+[1,2,3].forEach(function(n) {
+    console.log(n*2);
+});
+
+let array = [1,2,3].map(function(n) {
+    return(n*n);
+});
+
+console.log(array);
+
+let strArr = ['pen','book','AI','tool'].filter(function(str) {
+    if (str.length >= 3) {
+        return str;
+    }
+})
+
+console.log(strArr);
+
+let sum = [1,2,3].reduce(function(total, n) {
+    return total + n;
+}, 0);
+console.log(sum);
